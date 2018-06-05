53e71275f6ac5c566bda3b56308aaabec30001c7
Denis <Buridza0@gmail.com>
2018-06-05 23:01:11 +0300

    release

--- src/main/java/Statistic.java
+++ src/main/java/Statistic.java
@@ -6,55 +6,57 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.awt.*;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Statistic {
-
+    String FILE = "str";
 
     public static void main(String[] args) {
         List<Long> move = new ArrayList<Long>();
         List<Long> countTime = new ArrayList<Long>();
+        String str = (LocalDateTime.now().toString().replaceAll(":",".")+ " statistic.xlsx" );
 
         Statistic statistic = new Statistic();
+        statistic.FILE = str;
         statistic.filling(move, countTime);
+        for (int i = 0; i < countTime.size()-1; i++) {
+            if(countTime.get(i)==countTime.get(i+1)) countTime.remove(i+1);
+        }
 
         XSSFWorkbook workbook = new XSSFWorkbook();
-        workbook.createSheet("sheet for denis");
 
         statistic.toExcel(workbook, move, 0);
         statistic.toExcel(workbook, countTime, 1);
-
-
     }
 
-    Long size(final List<Long> list, final int index) {
-        long size;
-        if (list.size() > index + 1000)
-            size = index + 1000;
-        else size = list.size();
 
-        return size;
+
+    Long size(final List<Long> list, final int index) {
+        return (long)((list.size() > index + 1000)?(index + 1000): list.size());
     }
 
     void toExcel(final XSSFWorkbook workbook, final List<Long> list, final int columnIndex) {
 
         int i = 0;
         boolean status = false;
-        while (size(list, i)-1 < list.size() && !status) {
-            XSSFSheet sheet;
-            XSSFRow row;
-            XSSFCell cell;
+        XSSFSheet sheet;
+        XSSFRow row;
+        XSSFCell cell;
+        sheet = workbook.createSheet("sheet for denis" + columnIndex);
+        while (size(list, i) - 1 < list.size() && !status) {
+
             for (; i < size(list, i) && i < 1048576; i++) {
-                sheet = workbook.getSheet("sheet for denis");
+                sheet = workbook.getSheet("sheet for denis" + columnIndex);
                 row = sheet.createRow(i);
                 cell = row.createCell(columnIndex);
                 cell.setCellValue(list.get(i));
             }
             try {
-                workbook.write(new FileOutputStream("statistic.xlsx"));
+                workbook.write(new FileOutputStream(FILE));
                 System.out.println("Таблица " + columnIndex + " готова");
-                status=true;
+                status = true;
             } catch (IOException io) {
                 io.printStackTrace();
             }
@@ -63,19 +65,20 @@ public class Statistic {
 
     void filling(final List<Long> move, final List<Long> countTime) {
         System.out.println("запись начата");
-        int x;
-        int y;
-        x = MouseInfo.getPointerInfo().getLocation().x;
-        y = MouseInfo.getPointerInfo().getLocation().y;
+        int x = MouseInfo.getPointerInfo().getLocation().x;
+        int y = MouseInfo.getPointerInfo().getLocation().y;
+
         long startTime = System.currentTimeMillis();
         long timeMillis = startTime;
+
         while (timeMillis - startTime < 5000) {
             if ((x != MouseInfo.getPointerInfo().getLocation().x || y != MouseInfo.getPointerInfo().getLocation().y)) {
                 move.add(System.currentTimeMillis() - timeMillis);
-            } else if (System.currentTimeMillis()!=timeMillis) {
+                timeMillis = System.currentTimeMillis();
+            } else {
                 countTime.add(System.currentTimeMillis() - timeMillis);
             }
-            timeMillis = System.currentTimeMillis();
+
             x = MouseInfo.getPointerInfo().getLocation().x;
             y = MouseInfo.getPointerInfo().getLocation().y;
         }
